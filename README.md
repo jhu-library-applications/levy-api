d30a8526c05713bca424541532a77e17b1686f33
michelle.janowiecki@jhu.edu <michelle.janowiecki@jhu.edu>
2021-12-20 14:46:23 -0500

    Added script, updated readme

--- README.md
+++ README.md
@@ -7,20 +7,16 @@ Production Instance: https://levysheetmusic.mse.jhu.edu/
 ## Drupal settings required for these scripts
 
 Install [JSON:API module](https://www.drupal.org/docs/core-modules-and-themes/core-modules/jsonapi-module/api-overview)
- - Go to www.example.edu//admin/modules, navigate to Web Services, and check `JSON:API`
+ - Go to www.example.edu/admin/modules, navigate to Web Services, and check `JSON:API`
+ - Got to www.example.edu/admin/config/services/jsonapi and click "Accept all JSON:API create, read, update, and delete operations."
 
 Install [Paragraph Type Permissions sub-module](https://www.drupal.org/project/paragraphs)
- - Go to www.example.edu//admin/modules, navigate to Paragraphs, and check `Paragraphs Type Permissions`
-
-## File upload limits
-
-PDFs: 2MB
-
-Images: 2MB
+ - Go to www.example.edu/admin/modules, navigate to Paragraphs, and check `Paragraphs Type Permissions`
+ - Go to www.example.edu/admin/people/permissions, navigate to Paragraph Type Permissions, and check View Content for the paragraph types to change.
 
 ## Authentication
 
-In your post folder, please create a `secrets.py` file that contains the following information. This information will be used in the scripts that require authentication to post to Drupal.
+In your /post folder, please create a `secrets.py` file that contains the following information. This information will be used in the scripts that require authentication to post to Drupal.
 
 ```python
 username='username'
---
+++ get/getParagraph_collection_item_image.py
@@ -0,0 +1,70 @@
+import requests
+import pandas as pd
+import secrets
+
+# Your baseURL: https://example.com
+baseURL = 'https://levy-test.mse.jhu.edu/'
+type = 'jsonapi/paragraph/collection_item_image'
+
+username = secrets.username
+password = secrets.password
+
+# Authenicate to Drupal site, get token
+s = requests.Session()
+header = {'Content-type': 'application/json'}
+data = {'name': username, 'pass': password}
+session = s.post(baseURL+'user/login?_format=json', headers=header,
+                 json=data).json()
+token = session['csrf_token']
+status = s.get(baseURL+'user/login_status?_format=json').json()
+if status == 1:
+    print('authenticated')
+
+# Update headers for posting to Drupal
+s.headers.update({'Accept': 'application/vnd.api+json', 'X-CSRF-Token': token})
+
+
+# Function grabs name and uris from collection_name.
+def fetchData(data):
+    for count, term in enumerate(data):
+        itemDict = {}
+        attributes = term.get('attributes')
+        id = term.get('id')
+        restrict = attributes.get('field_restricted')
+        parent_id = attributes.get('parent_id')
+        itemDict['restrict'] = restrict
+        itemDict['id'] = id
+        itemDict['parent_id'] = parent_id
+        if restrict is True:
+            allItems.append(itemDict)
+
+
+# Loop through item and grabs metadata, chuck into DataFrame.
+allItems = []
+more_links = True
+nextList = []
+while more_links:
+    if not nextList:
+        r = s.get(baseURL+type+'?page[limit=50]').json()
+    else:
+        next = nextList[0]
+        r = s.get(next).json()
+    data = r.get('data')
+    print(len(data))
+    fetchData(data)
+    nextList.clear()
+    links = r.get('links')
+    nextDict = links.get('next')
+    if nextDict:
+        next = nextDict.get('href')
+        nextList.append(next)
+    else:
+        break
+print('')
+
+
+all_items = pd.DataFrame.from_dict(allItems)
+print(all_items.head)
+
+# Create CSV for new DataFrame.
+all_items.to_csv('allParagraphs_collecction_item_image.csv', index=False)
