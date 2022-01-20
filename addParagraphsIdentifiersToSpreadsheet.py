2e0f3283356a810b049af28e761da2ec2148837f
michelle.janowiecki@jhu.edu <michelle.janowiecki@jhu.edu>
2022-01-20 09:49:15 -0500

    Add script + update paragraph scripts

---
+++ addParagraphsIdentifiersToSpreadsheet.py
@@ -0,0 +1,20 @@
+import pandas as pd
+
+filename = 'logOfParagraphCollectionName.csv'
+df = pd.read_csv(filename, header=0)
+
+filename2 = '01_GottesmanFinalMetadata_2021-12-03.csv'
+new_items = pd.read_csv(filename2)
+
+df.drop(['link', 'creator_role_id', 'levy_collection_names_id'], axis=1, inplace=True)
+pivoted = pd.pivot_table(df, index=['fileIdentifier'], values=['paragraph_id'],
+                         aggfunc=lambda x: '|'.join(str(v) for v in x if pd.notna(v)))
+df = pd.DataFrame(pivoted)
+df = df.reset_index()
+
+
+updated = pd.merge(new_items, df, how='left', on=['fileIdentifier'], suffixes=('_1', '_2'))
+print(updated.head)
+filename2 = filename2[3:]
+filename2 = '02_'+filename2
+updated.to_csv(filename2, index=False)
--- post/postParagraph_collection_name.py
+++ post/postParagraph_collection_name.py
@@ -3,7 +3,6 @@ import secrets
 import json
 import pandas as pd
 import time
-from datetime import datetime
 import os
 
 username = secrets.username
@@ -36,7 +35,7 @@ s.headers.update({'Accept': 'application/vnd.api+json', 'Content-Type':
 
 
 # Open file CSV as DataFrame
-filename = 'filenames.csv'
+filename = 'paragraph_collection_namesToAdd.csv'
 df = pd.read_csv(filename)
 
 allItems = []
@@ -51,8 +50,8 @@ for index, row in df.iterrows():
                   'id': names_id}}
     field_roles = {'data': {'type': 'taxonomy_term--creator_r',
                    'id': creator_role}}
-    paragraph_type = {'data': {'type':'paragraphs_type--paragraphs_type',
-                    'id':'36170ce3-1d71-462d-95d0-01167a11e679'}
+    paragraph_type = {'data': {'type': 'paragraphs_type--paragraphs_type',
+                      'id': '36170ce3-1d71-462d-95d0-01167a11e679'}}
     relationships = {'paragraph_type': paragraph_type, 'field_name': field_name,
                      'field_roles': field_roles}
     data = {'data': {'type': 'paragraph--collection_name',
@@ -60,18 +59,20 @@ for index, row in df.iterrows():
     metadata = json.dumps(data)
     post = s.post(baseURL+type, data=metadata, cookies=s.cookies).json()
     data = post.get('data')
-    id = data.get('id')
-    link = data['links']['self']['href']
-    print(id, link)
-    row['paragraph_id'] = id
-    row['link'] = link
+    if data:
+        id = data.get('id')
+        link = data['links']['self']['href']
+        print(id, link)
+        row['paragraph_id'] = id
+        row['link'] = link
+    else:
+        row['link'] = 'error'
     allItems.append(row)
 
 
 # Convert results to DataFrame, export as CSV
 log = pd.DataFrame.from_dict(allItems)
-dt = datetime.now().strftime('%Y-%m-%d')
-newFile = 'logOfParagraphCollectionName_'+dt+'.csv'
+newFile = 'logOfParagraphCollectionName.csv'
 fullname = os.path.join(directory, newFile)
 log.to_csv(fullname)
 
