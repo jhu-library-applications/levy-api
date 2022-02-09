f7e35f2b652779658cb3327eca34d60cdaf3dbe9
Michelle Janowiecki <32551917+mjanowiecki@users.noreply.github.com>
2022-02-09 09:32:59 -0500

    Update addParagraphsIdentifiersToSpreadsheet.py

--- metadata-spreadsheet/addParagraphsIdentifiersToSpreadsheet.py
+++ metadata-spreadsheet/addParagraphsIdentifiersToSpreadsheet.py
@@ -1,5 +1,15 @@
 import pandas as pd
 import os
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-f', '--file')
+args = parser.parse_args()
+
+if args.file:
+    metadata_file = args.file
+else:
+    metadata_file = input('Enter filename (including \'.csv\'): ')
 
 path = os.getcwd()
 dir = os.path.dirname(path)
@@ -8,8 +18,7 @@ logs = os.path.join(dir, 'logs/')
 filename = os.path.join(logs, 'logOfParagraphCollectionName.csv')
 df = pd.read_csv(filename, header=0)
 
-filename2 = '01_GottesmanFinalMetadata_test.csv'
-new_items = pd.read_csv(filename2, header=0)
+new_items = pd.read_csv(metadata_file, header=0)
 
 df.drop(['link', 'creator_role_id', 'levy_collection_names_id'], axis=1, inplace=True)
 pivoted = pd.pivot_table(df, index=['fileIdentifier'], values=['paragraph_id'],
@@ -20,6 +29,6 @@ df = df.reset_index()
 
 updated = pd.merge(new_items, df, how='left', on=['fileIdentifier'], suffixes=('_1', '_2'))
 print(updated.head)
-filename2 = filename2[3:]
-filename2 = '02_'+filename2
-updated.to_csv(filename2, index=False)
+metadata_file = metadata_file[3:]
+metadata_file = '02_'+metadata_file
+updated.to_csv(metadata_file, index=False)
